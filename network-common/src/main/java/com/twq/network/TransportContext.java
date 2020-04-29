80883d8c5726d54b5a69e8a8e6102417b0d2ca2a
Jeffy Tang <tangwq_ok@163.com>
2020-04-29 14:16:27 +0800

    往 pipeline 中添加编解码器

--- network-common/src/main/java/com/twq/network/TransportContext.java
+++ network-common/src/main/java/com/twq/network/TransportContext.java
@@ -2,11 +2,19 @@ package com.twq.network;
 
 import com.twq.network.client.TransportClient;
 import com.twq.network.client.TransportClientFactory;
+import com.twq.network.protocol.MessageDecoder;
+import com.twq.network.protocol.MessageEncoder;
 import com.twq.network.server.TransportChannelHandler;
 import com.twq.network.server.TransportServer;
 import io.netty.channel.socket.SocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TransportContext {
+    private static final Logger logger = LoggerFactory.getLogger(TransportContext.class);
+
+    private static final MessageEncoder ENCODER = MessageEncoder.INSTANCE;
+    private static final MessageDecoder DECODER = MessageDecoder.INSTANCE;
 
     public TransportClientFactory createClientFactory() {
         return new TransportClientFactory(this);
@@ -21,10 +29,19 @@ public class TransportContext {
     }
 
     public TransportChannelHandler initializePipeline(SocketChannel channel) {
-        TransportChannelHandler channelHandler = createChannelHandler(channel);
-        channel.pipeline()
-                .addLast("handler", channelHandler);
-        return channelHandler;
+
+        try {
+            TransportChannelHandler channelHandler = createChannelHandler(channel);
+            channel.pipeline()
+                    .addLast("encoder", ENCODER)
+                    .addLast("decoder", DECODER)
+                    .addLast("handler", channelHandler);
+            return channelHandler;
+        } catch (RuntimeException e) {
+            logger.error("Error while initializing Netty pipeline", e);
+            throw e;
+        }
+
     }
 
 
